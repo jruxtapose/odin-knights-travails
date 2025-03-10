c9af3d5c7d5b534667f980e754658821565a0168
dhauke89 <18323741+dhauke89@users.noreply.github.com>
2025-03-10 17:37:54 -0400

    Refactor node constructor.

--- node.js
+++ node.js
@@ -1,25 +1,24 @@
-export default class Node{
-    // Checks that node is within board before creation.
-    constructor(vertical, horizontal){
-        if(vertical >= 0 && vertical <= 7) {
-            this.vertical = vertical;
-        } else {
-            throw new Error (`Vertical = ${vertical} is outside the board.`)
-        }
-        if(horizontal >= 0 && horizontal <= 7) {
-            this.horizontal = horizontal;
-        } else {
-            throw new Error (`Horizontal = ${horizontal} is outside the board.`)
-        }
-    }
-    // Returns location of node.
-    getLocation(){
-        return [this.vertical, this.horizontal];
-    }
+export default class Node {
+  // Checks that node is within board before creation.
+  constructor(vertical, horizontal) {
+    if (vertical < 0 || vertical > 7)
+      throw new Error(`Vertical = ${vertical} is outside the board.`);
+    if (horizontal < 0 || horizontal > 7)
+      throw new Error(`Horizontal = ${horizontal} is outside the board.`);
+    this.vertical = vertical;
+    this.horizontal = horizontal;
+  }
+  // Returns location of node.
+  getLocation() {
+    return [this.vertical, this.horizontal];
+  }
 
-    // Compares node to other node.
-    equals(otherNode) {
-        if (otherNode === null) return false;
-        return this.vertical === otherNode.vertical && this.horizontal === otherNode.horizontal;
-    }
+  // Compares node to other node.
+  equals(otherNode) {
+    if (otherNode === null) return false;
+    return (
+      this.vertical === otherNode.vertical &&
+      this.horizontal === otherNode.horizontal
+    );
+  }
 }
